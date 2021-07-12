40f301f037dcf15ec477c1859e44e7cc31e74b28
joseluisserna <jose.sernas@alumnos.udg.mx>
2021-07-12 00:30:29 -0500

    react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import Main from "./components/MainComponent"; //Import the Main component from MainComponent
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 //Render the main component
 class App extends Component {
   render() {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
